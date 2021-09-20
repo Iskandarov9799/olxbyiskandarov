bb7ad742809b6bd92cf86656a5187cefd1617bae
Iskandarov9799 <qudrat.iskandarov98@gmail.com>
2021-09-20 16:47:58 +0500

    add middlewares and settings

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
 require("dotenv").config()
 
+const cookieParser = require("cookie-parser");
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
+const path = require("path");
+const PORT = process.env.PORT || 1998;
+
+async function server() {
+    const app = express();
+    // middlewares 
+    app.use(express.json());
+    app.use(
+        express.urlencoded({
+            extended: true
+        })
+    );
+    app.use(morgan("dev"));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, "src", "public")));
+
+
+    // settings
+    app.set("view engine", "ejs");
+    
+
+}
+    
+
+
